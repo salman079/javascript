104b5213a30fdb9fe9e9585b7ac69f8bc93046b2
salmansiddiqui <salman079@gmail.com>
2020-08-29 01:05:16 +0300

    Math and date

--- date.js
+++ date.js
@@ -1,9 +1,21 @@
 var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 var now = new Date();
 var theDay = now.getDay();
 var nameOfToday = dayNames[theDay];
 console.log(nameOfToday);
 console.log(Date()+"==="+ new Date().getDay());
-console.log(dayNames[new Date().getDay()]);
+console.log("Todays day is "+dayNames[new Date().getDay()]);
+console.log("Todays Month is "+monthNames[new Date().getMonth()]);
+
 console.log("Days to birthday "+ (new Date("August 21, 2021").getDate() - new Date().getDate()));
 console.log(new Date("August 21, 2021").getDate());
+console.log(new Date().getFullYear());
+console.log(new Date().getMonth());
+console.log(new Date().getDate());
+console.log(new Date().getDay());
+console.log(new Date().getHours());
+console.log(new Date().getMinutes());
+console.log(new Date().getSeconds());
+console.log(new Date().getTime());
+//setFullyear() and similar to set the date etc
--- math.js
+++ math.js
@@ -3,7 +3,12 @@ var numRound = Math.round(a);
 console.log(numRound);
 console.log("using Math ceil on .000001:" + Math.ceil(0.000001));
 console.log("using Math floor on .99999:" + Math.floor(0.99999));
+var abc = 0.65978392324213;
+console.log("using Math toFixed function to round 3 places of decimal on " + abc +" is "+ abc.toFixed(3));
+
 var bigDecimal = Math.random();
 var improvedNum = (bigDecimal * 6)+1;
 var numberOfStars = Math.floor(improvedNum);
 console.log("random number 1 to 6:" + bigDecimal+ " " + improvedNum + " "  + numberOfStars+ " " );
+console.log("Maths Functions");
+console.log("Math.pow() \nMath.sqrt() \nMath.abs() \nMath.sin() \nMath.cos() \nMath.min() \nMath.max() \nMath.exp() \nMath.log() \n");
