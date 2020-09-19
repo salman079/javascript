8845e68d69ed6ce7f7d31e027edaf92c78bb8f37
salmansiddiqui <salman079@gmail.com>
2020-09-19 11:27:41 +0300

    ES6

--- aa copy 2.js
+++
@@ -1,3 +0,0 @@
-document.write(2+5);
-a = 9+9;
-document.write(a);
---
+++ array_destructuring in ES6.js
@@ -0,0 +1,7 @@
+let proglang = ['JS', 'Python', 'React', 'Rust', 'Docker', 'Kubernetes'];
+let [lan1, lan2, lan3, lan4, lan5, lan6] = proglang;
+document.write(`My 5th language is ${lan5}`);
+document.getElementById('return1').innerHTML=(`My 2nd language is ${lan2}`);
+document.write(`and last one is ${lan6}`);
+// for(var i=0; i<100000000000;i++){};
+document.getElementById('return1').innerHTML=(`and changed from 2nd language to 1st ${lan1}`);
