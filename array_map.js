e5800ae4d376e48f188b3202d225b5dd357ef2b1
salmansiddiqui <salman079@gmail.com>
2020-09-27 22:32:44 +0300

    to check ...Array

--- array_map.js
+++ array_map.js
@@ -1,9 +1,9 @@
 const oldArr = ['salman', 'aman', 'saima', 'samina', 'zaki'];
 document.write("Old Array <br>");
-document.write(oldArr+'<br>');
+document.write(oldArr + '<br>');
 
-const newArr = oldArr.map(function(newValue,p) {
-    return p+1 +": "+newValue;
+const newArr = oldArr.map(function (newValue, p) {
+    return p + 1 + ": " + newValue;
 });
 document.write("<h3>Array with map function </h3> <br>");
 document.write(newArr);
@@ -11,31 +11,31 @@ console.log(newArr);
 document.write("<hr>");
 
 const studentdata = [
-    {id: 1, name: "Tooba", class: "3b"},
-    {id: 2, name: "Abdullah", class: "2a"},
-    {id: 3, name: "Hassan", class: "kg"}
+    { id: 1, name: "Tooba", class: "3b" },
+    { id: 2, name: "Abdullah", class: "2a" },
+    { id: 3, name: "Hassan", class: "kg" }
 ];
-document.write("The class of "+studentdata[1].name+" is "+studentdata[1].class);
+document.write("The class of " + studentdata[1].name + " is " + studentdata[1].class);
 document.write("<hr>");
 
-const stddata1 = studentdata.map(function(newValue){
+const stddata1 = studentdata.map(function (newValue) {
     return newValue.name;
 }
 );
 document.write("calling name using function:" + stddata1);
 document.write("<hr>");
 
-const stddata2 = studentdata.map((newValue2)=> {
+const stddata2 = studentdata.map((newValue2) => {
     return newValue2.class;
 }
 );
 document.write("calling class using => :" + stddata2);
 document.write("<hr>");
-document.write("Names of kids are "+ stddata1 + " and study in class"+ stddata2);
+document.write("Names of kids are " + stddata1 + " and study in class" + stddata2);
 document.write("<hr>");
 
-const stddata3 = studentdata.map((newValue3)=> {
-    return "Name of the kid is "+newValue3.name+" and study in class: "+ newValue3.class+"<br>";
+const stddata3 = studentdata.map((newValue3) => {
+    return "Name of the kid is " + newValue3.name + " and study in class: " + newValue3.class + "<br>";
 }
 );
 document.write(stddata3);
@@ -53,25 +53,31 @@ document.write("<hr>");
 
 const oldArr1 = [
     {
-    Name:'salman1111', 
-    Id: "Ab11",
-    Class: "2b"
-},
-{
-    Name:'Abdullah1111', 
-    Id: "Zx121",
-    Class: "3A"
-},
-{
-    Name:'Hassan1111', 
-    Id: "pqr121",
-    Class: "AA"
-}];
+        Name: 'salman1111',
+        Id: "Ab11",
+        Class: "2b"
+    },
+    {
+        Name: 'Abdullah1111',
+        Id: "Zx121",
+        Class: "3A"
+    },
+    {
+        Name: 'Hassan1111',
+        Id: "pqr121",
+        Class: "AA"
+    }];
 oldArr1.map((datas) => {
-    nam=datas.Name;
-    iid=datas.Id;
-    cclass=datas.Class;
-    
-    document.write("name is <strong>" + nam+ "</strong> and having ID # of <strong>" + iid +"</strong> and study in class <strong>" + cclass+"</strong>");
+    nam = datas.Name;
+    iid = datas.Id;
+    cclass = datas.Class;
+
+    document.write("name is <strong>" + nam + "</strong> and having ID # of <strong>" + iid + "</strong> and study in class <strong>" + cclass + "</strong>");
     document.write("<br>");
 });
+
+newArrays = ['a', 'b', 'c'];
+newArrays2 = [...newArrays, 'x', 'y', 'z'];
+document.write(` This is newArray2 ${newArrays2}`);
+newArrays3 = [...newArrays2,'saloo',...newArrays,'amao'];
+document.write(` This is newArray3 ${newArrays3}`);
